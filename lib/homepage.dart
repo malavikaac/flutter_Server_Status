3e13816ccca06c65bf0394507314eeb75e5e909b
malavikaac <acmalavika@gmail.com>
2023-06-09 17:02:41 +0100

    model

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,8 @@
- //TODO Implement this library.
- import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+import 'model.dart';
 
 class ServerStatusWidget extends StatefulWidget {
   @override
@@ -9,103 +11,58 @@ class ServerStatusWidget extends StatefulWidget {
 
 class _ServerStatusWidgetState extends State<ServerStatusWidget> {
   bool isServerOn = false;
+  var result;
 
-  Future<void> checkServerStatus() async {
-    try {
-      final response = await http.get(Uri.parse('http://your-server-url.com'));
-      
-      if (response.statusCode == 200) {
-        setState(() {
-          isServerOn = true;
-        });
-      } else {
-        setState(() {
-          isServerOn = false;
-        });
-      }
-    } catch (e) {
-      setState(() {
-        isServerOn = false;
-      });
+  Future<ServerModel?> dashboardDetails() async {
+    final http.Response response = await http.get(
+      Uri.parse('http://api.atrehealthtech.com:1400/server'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      result = json.decode(response.body);
+      print(response.body);
+      return ServerModel.fromJson(result);
+    } else {
+      return ServerModel.fromJson(result);
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle:true,backgroundColor: Color.fromARGB(255, 217, 248, 247),
-      title:Text("Server Status",style: TextStyle(color: Colors.brown),)),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: checkServerStatus,
-              child: Text('Check Server Status'),
+      appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 217, 248, 247),
+          title: Text(
+            "Server Status",
+            style: TextStyle(color: Colors.brown),
+          )),
+      body: FutureBuilder<ServerModel>(
+        //future: Future(() => dashboardDetails()),
+        builder: (context, snapshot) => Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: dashboardDetails,
+                child: Text(snapshot.data.toString()),
+              ),
             ),
-          ),
-             SizedBox(height: 16.0),
-          Text(
-            isServerOn ? 'Server is ON' : 'Server is OFF',
-            style: TextStyle(fontSize: 20.0),
-          ),
-        ],
+            SizedBox(height: 16.0),
+            Column(
+              children: [
+                Text(
+                  isServerOn ? 'Server is ON' : 'Server is OFF',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-      
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'server_status.dart';
-
-// void main() {
-//   runApp(ServerStatusApp());
-// }
-
-// class ServerStatusApp extends StatefulWidget {
-//   @override
-//   _ServerStatusAppState createState() => _ServerStatusAppState();
-// }
-
-// class _ServerStatusAppState extends State<ServerStatusApp> {
-//   bool _isOnline = false;
-//   late ServerStatus _serverStatus;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _serverStatus = ServerStatus('http://your-server-url.com'); // Replace with your server URL
-//     _checkServerStatus();
-//   }
-
-//   Future<void> _checkServerStatus() async {
-//     final isOnline = await _serverStatus.isServerOnline();
-//     setState(() {
-//       _isOnline = isOnline;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('Server Status'),
-//         ),
-//         body: Center(
-//           child: _isOnline
-//               ? Text(
-//                   'Server is online',
-//                   style: TextStyle(fontSize: 20, color: Colors.green),
-//                 )
-//               : Text(
-//                   'Server is offline',
-//                   style: TextStyle(fontSize: 20, color: Colors.red),
-//                 ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
---
+++ lib/model.dart
@@ -0,0 +1,18 @@
+class ServerModel {
+  String? status;
+  String? message;
+
+  ServerModel({this.status, this.message});
+
+  ServerModel.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    message = json['message'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['status'] = this.status;
+    data['message'] = this.message;
+    return data;
+  }
+}
