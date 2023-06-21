53779820f3569751b08c65da70ff6412ba508c30
malavikaac <acmalavika@gmail.com>
2023-06-21 17:14:06 +0100

    feat - Created a Api Service , provider

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -6,11 +6,10 @@ import 'package:http/http.dart' as http;
 
 class ApiService extends ChangeNotifier {
   Future<ServerConnectionModel?> getServer() async {
-    final http.Response response = await http.post(
-        Uri.parse("https://test-atre-server-v2.up.railway.app/server"),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        });
+    final http.Response response =
+        await http.post(Uri.parse(""), headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    });
     try {
       if (response.statusCode == 200) {
         final result = jsonDecode(response.body);
